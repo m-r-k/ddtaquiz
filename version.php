59d1142a84895d293f42b9cb6fbf7e18c096af2a
Unknown <k.schwinn93@gmail.com>
2019-01-15 23:02:00 +0100

    DDTAQuiz_DirectFeedback
    version control

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ddtaquiz';
-$plugin->version = 2018040900;
+$plugin->version = 2019011509;
 $plugin->release = 'v0.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
